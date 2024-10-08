c6eb706039a9f25848d86d305b79d68e5b8e1643
Andreas <alehmann42@gmail.com>
2024-10-08 22:43:29 +0200

    1. Webkonferenz

--- app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/MainActivity.java
+++ app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/MainActivity.java
@@ -3,14 +3,26 @@ package org.dieschnittstelle.mobile.android.skeleton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.dieschnittstelle.mobile.android.skeleton.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView welcomeText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView welcomeText = findViewById(R.id.welcomeText);
+        welcomeText.setText(R.string.welcome_message_alternative);
+
+        welcomeText.setOnClickListener((view) ->
+                Toast.makeText(MainActivity.this,"HELLO AGAIN",Toast.LENGTH_SHORT).show()
+        );
     }
 }
