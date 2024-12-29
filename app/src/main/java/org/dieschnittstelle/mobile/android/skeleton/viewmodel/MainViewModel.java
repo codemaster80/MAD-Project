46c037f8f5996a73f24aba0a2b5361fbe04aae0a
Andreas <alehmann42@gmail.com>
2024-12-29 10:49:51 +0100

    Failed mailcheck error message duration change to infinited #2

--- app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/viewmodel/MainViewModel.java
+++ app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/viewmodel/MainViewModel.java
@@ -1,38 +1,20 @@
 package org.dieschnittstelle.mobile.android.skeleton.viewmodel;
-import static androidx.core.content.ContextCompat.startActivity;
 
-import android.content.Intent;
-import android.util.Log;
-
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
-
-import com.google.firebase.firestore.MemoryLruGcSettings;
-
-import org.dieschnittstelle.mobile.android.skeleton.TaskListViewActivity;
-import org.dieschnittstelle.mobile.android.skeleton.model.ITaskDatabaseOperation;
 import org.dieschnittstelle.mobile.android.skeleton.model.IUserDatabaseOperation;
-import org.dieschnittstelle.mobile.android.skeleton.model.LocalTaskDatabaseOperation;
-import org.dieschnittstelle.mobile.android.skeleton.model.LocalUserDatabaseOperation;
-import org.dieschnittstelle.mobile.android.skeleton.model.User;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class MainViewModel extends ViewModel {
         private IUserDatabaseOperation userDBOperation;
         private MutableLiveData<Boolean> isUserAuthenticated = new MutableLiveData<>(false);
         private MutableLiveData<Boolean> isUsernameNotAnEMail = new MutableLiveData<>(false);
         private MutableLiveData<Boolean> isUserAuthenticationFailed = new MutableLiveData<>(false);
-        private User user;
         public String email;
         public String pwd;
 
         public void authenticateUser() {
             if (android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
                 new Thread(() -> {
-                    // Log.i("Login", String.valueOf(userDBOperation.authenticateUser("a@b.de", "test")));
                     if (userDBOperation.authenticateUser(email, pwd)) {
                         isUserAuthenticated.postValue(true);
                     } else {
